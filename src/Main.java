d65d484efcee3f208390e6430a4e5382ceff2b2b
Passiondown <a9979a458@gmail.com>
2024-04-02 10:40:48 +0800

    the learning of public

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {//main 类
+    public static void main(String[] args) {
+        System.out.println("Hello world你!");
+    }//main方法
+}
+class dog {//main 类
+    public static void main(String[] args) {
+        System.out.println("你是狗!");
+    }//main方法
+}
+class pig {//main 类
+    public static void main(String[] args) {
+        System.out.println("你是猪!");
+    }//main方法
+}
